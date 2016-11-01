41bf84bae9db7a87a0d3fd82e89948a7c933e9e7
Michael Miscampbell <mmiscampbell@gcdtech.com>
2016-11-01 12:04:59 +0000

    Removing MultiSelect View

--- src/SelectionControls/MultiSelect/MultiSelect.php
+++ src/SelectionControls/MultiSelect/MultiSelect.php
@@ -2,13 +2,14 @@
 
 namespace Rhubarb\Leaf\Controls\Common\SelectionControls\MultiSelect;
 
+use Rhubarb\Leaf\Controls\Common\SelectionControls\DropDown\DropDownView;
 use Rhubarb\Leaf\Controls\Common\SelectionControls\SelectionControl;
 
 class MultiSelect extends SelectionControl
 {
     protected function getViewClass()
     {
-        return MultiSelectView::class;
+        return DropDownView::class;
     }
 
     protected function supportsMultipleSelection()
