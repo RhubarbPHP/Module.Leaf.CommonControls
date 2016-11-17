6194002a213695071b8b8f8f3123bbb8057647d3
NiallSmyth <nsmyth@gcdtech.com>
2016-11-17 13:03:43 +0000

    Added settable input type for textbox leaf

--- src/Text/TextBox.php
+++ src/Text/TextBox.php
@@ -6,13 +6,26 @@ use Rhubarb\Leaf\Leaves\Controls\Control;
 
 class TextBox extends Control
 {
+    /** @var  TextBoxModel */
+    protected $model;
+    
     protected function getViewClass()
     {
         return TextBoxView::class;
     }
 
+    protected function createModel()
+    {
+        return new TextBoxModel();
+    }
+
     public function setMaxLength($maxLength)
     {
         $this->addHtmlAttribute("maxlength", $maxLength);
+    }
+    
+    public function setInputType($inputType)
+    {
+        $this->model->inputType = $inputType;
     }
 }
